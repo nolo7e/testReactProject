a609e3d5e9779f2e534cdd805af61ff01bf27eac
na_slone <ivanov1571men@gmail.com>
2023-08-02 18:53:39 +0300

    aa

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -10,4 +10,9 @@ import axios from "axios";
             });
             return response;
     }
+
+    static async getById(id){
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+        return response;
+}
  }
--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import './styles/App.css';
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Navigate, Route, Router, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
+import Navbar from "./components/UI/Navbar/Navbar";
+import Error from "./pages/Error";
+import AppRouter from "./components/AppRouter";
 
 
 function App() {
   return(
     <BrowserRouter>
-      <div className="navbar">
-      
-      </div>
-      <Routes>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/posts" element={<Posts/>}/>
-      </Routes>
+      <Navbar/>
+      <AppRouter/>
     </BrowserRouter>
   );
 }
